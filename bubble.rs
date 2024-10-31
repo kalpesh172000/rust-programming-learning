517ebcdca5c49a7bb1bb5190011a9dbef6071e3e
kalpesh <kalpesh200071@gmail.com>
2024-10-31 19:14:12 +0530

    bubble sort in rust

--- bubble.rs
+++ bubble.rs
@@ -1,11 +1,21 @@
+use std::io::Write;
+fn main() {
+    let mut nums: [u16; 10] = [4, 12, 7, 3, 5, 9, 6, 8, 2, 1];
+    bubble(&mut nums);
+    for i in nums.iter() {
+        print!("{} ", i);
+        let _ = std::io::stdout().flush();
+    }
+    println!();
+}
+fn bubble(nums: &mut [u16]) {
+    let size = nums.len();
 
-
-fn main()
-{
-    let mut nums:[u16; 10] = [4, 12, 7, 4, 5, 9, 7, 8, 2, 1]; 
-
-    bubble_sort(&mut nums);
-    for i in nums.iter(){
-        println!("{}", i);
+    for i in 0..size {
+        for j in 0..size - i - 1 {
+            if nums[j] > nums[j+1] {
+                (nums[j], nums[j+1]) = (nums[j+1], nums[j]);
+            }
+        }
     }
 }
